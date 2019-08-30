b94f20bb5e483581e91958572877cbaa5cded72d
napakorn <61030050@kmitl.ac.th>
2019-08-30 21:57:48 +0700

    3rd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -14,6 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2560 semester 1, I start my programmer life");
             Console.WriteLine("Year 2560 semester 2, I completed my first game with unity");
+            Console.WriteLine("Year 2561 semester 1, I have C# skill in intermediate");
         }
     }
 }
